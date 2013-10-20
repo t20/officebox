323ec48c4fa8cbef6eb71b007a7eb41b7af3e604
Bharad <barbi.bruce@gmail.com>
2013-10-19 20:00:01 -0700

    admin stub route and template

--- app.py
+++ app.py
@@ -31,6 +31,11 @@ def register():
     return render_template('register.html')
 
 
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 0))
     if port:
