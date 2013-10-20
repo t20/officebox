e11b57a7c5fcbc52de907ad602a019cfcefca22b
Bharad <barbi.bruce@gmail.com>
2013-10-19 20:17:23 -0700

    adding method to create product. optimized request calls

--- tmforum.py
+++ tmforum.py
@@ -1,17 +1,22 @@
 import requests
 
 
+def create_product(params):
+    URL = 'http://env-4126955.jelastic.servint.net/DSProductCatalog/api/productOffering'
+    return get_request(URL, params)
+
+
 def get_product(params):
     URL = 'http://env-4126955.jelastic.servint.net/DSProductCatalog/api/productOffering'
-    response = requests.get(URL, params=params)
-    if response.status_code != 200:
-        return None
-    response = response.json()
-    return response
+    return get_request(URL, params)
 
 
 def create_order(params):
     URL = 'http://env-4126955.jelastic.servint.net:8080/DSProductOrdering/api/productOrder'
+    return get_request(URL, params)
+
+
+def get_request(URL, params):
     response = requests.get(URL, params=params)
     if response.status_code != 200:
         return None
