a4eb6dbfbeb8cd7e03e3d4bd7229448adce79b22
Bharad <barbi.bruce@gmail.com>
2013-10-19 18:12:06 -0700

    adding heroku stuff - procfile and req

---
+++ settings.py
@@ -0,0 +1,16 @@
+class Config(object):
+    """Default config"""
+    DEBUG = True
+
+    APP_SECRET_KEY = '\x01\xd2+\x1c\x9b>\xdf\x85\xe65z\xba\xaa\x89\xfc\x18\xa4D\xb3\xe2\xa8\x1fP\x8d'
+    
+    REDIS_DB = 0
+    REDIS_PORT = 6379
+    REDIS_HOST = 'localhost'
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+    APP_SECRET_KEY = ''
+
+
